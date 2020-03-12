9ea8e6b95cc2f97fd94824b25599f1ebc090f456
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-12 17:00:51 +0100

    DRY in authService.js

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,28 +3,20 @@ import { AxiosResponse } from 'axios';
 
 
 const loginEndPoint = '/login';
-const loginStatus = 200;
-
 const refreshTokenEndPoint = '/refreshToken';
-const refreshTokenStatus = 200;
 
-const contentType = 'multipart/form-data';
 const headers = {
-    'content-type': contentType
+    'content-type': 'multipart/form-data'
 }
 
 export default {
     async login(formData: FormData, callback: Function, errorCallback: Function) {
-        const response: AxiosResponse = await Service.post(loginEndPoint, formData, {
-            headers
-        });
-        (response.status === loginStatus) ? callback(response) : errorCallback(response);
+        const response: AxiosResponse = await Service.post(loginEndPoint, formData, { headers });
+        (response.status === 200) ? callback(response) : errorCallback(response);
     },
-    
+
     async refreshToken(callback: Function, errorCallback: Function) {
-        const response: AxiosResponse = await Service.post(refreshTokenEndPoint, {}, {
-            withCredentials: true
-        });
-        (response.status === refreshTokenStatus) ? callback(response) : errorCallback(response);
+        const response: AxiosResponse = await Service.post(refreshTokenEndPoint, {}, { withCredentials: true });
+        (response.status === 200) ? callback(response) : errorCallback(response);
     }
 }
