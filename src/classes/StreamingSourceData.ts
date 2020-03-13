e4aa3d3aa78f29055274d4dc7766e3b4130f3ef9
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-13 17:24:53 +0100

    Create StreamingSourceData.ts

---
+++ src/classes/StreamingSourceData.ts
@@ -0,0 +1,21 @@
+export interface StreamingSourceDataInterface {
+    id?: string;
+    name: string;
+    path: string;
+}
+
+export default class StreamingSourceData {
+    private _id?: string;
+    name: string;
+    path: URL;
+
+    get id() {
+        return this._id;
+    }
+
+    constructor({ id, name, path }: StreamingSourceDataInterface) {
+        this._id = id;
+        this.name = name;
+        this.path = new URL(path);
+    }
+}
