ea2d708bb1300a7c54f60256a4b1c3eee6ea09d0
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-19 13:10:07 +0100

    Basic styling setup

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,17 @@ export default class App extends Vue {
 </script>
 
 <style lang="sass">
+@import "@/styles/variables.sass"
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900&display=swap')
 
+body
+    background-color: $dark-color
+    color: $white-color
+    font-family: 'Source Sans Pro', sans-serif
+    margin: 0
+    padding: 0  
+
+a
+    color: inherit
+    text-decoration: unset
 </style>
