c7768f56dc18623d85dd81bc3fc9feeb1c010e03
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-22 15:00:13 +0100

    Update styling & create basic sorting

--- src/views/Studio/Series.vue
+++ src/views/Studio/Series.vue
@@ -2,20 +2,23 @@
     <div>
         <ul class="series-list">
             <div class="series-list__header">
-                <div class="series-list__header__info">Tytuł</div>
+                <div class="series-list__header__info" @click="titleSorting = !titleSorting">
+                    Tytuł
+                    <v-material-icon>sort</v-material-icon>
+                </div>
                 <div class="series-list__header__status">Status</div>
                 <div class="series-list__header__episodes">Odcinki</div>
             </div>
             <li
                 class="series-list__element"
                 :class="{'series-list__element--not-active': (activeSeries !== null && _series.id !== activeSeries)}"
-                v-for="_series in series"
+                v-for="_series in seriesSorted"
                 :key="_series.id"
                 @mouseenter="activeSeries=_series.id"
                 @mouseleave="activeSeries=null"
             >
                 <div class="series-list__element__info">
-                    <h3 class="series-list__element__info__name">{{_series.name}}</h3>
+                    <div class="series-list__element__info__name">{{_series.name}}</div>
                     <div
                         class="series-list__element__info__description"
                     >{{_series.shortDescription}}</div>
@@ -25,7 +28,7 @@
                     class="series-list__element__episodes"
                 >{{_series.availableEpisodes}} / {{_series.allEpisodes}}</div>
                 <div>
-                    <i class="material-icons">edit</i>
+                    <i class="material-icons" style="font-size: 1em">edit</i>
                 </div>
             </li>
         </ul>
@@ -38,13 +41,27 @@ import Component from "vue-class-component";
 import seriesService from "@/services/seriesService";
 import { AxiosResponse } from "axios";
 import SeriesData, { SeriesDataInterface } from "../../classes/SeriesData";
-import { SeriesResponse } from "@/services/responseInteraces";
 import ServiceError from "../../services/errors/ServiceError";
 import { Route, NavigationGuard, Next } from "vue-router";
 import SeriesStatus from "@/enums/SeriesStatus";
+import { LoadingBus } from "@/events/eventBus";
+
+import MaterialIcon from "@/components/Icons/MaterialIcon.vue";
 
 @Component({
+    components: {
+        "v-material-icon": MaterialIcon
+    },
+    computed: {
+        seriesSorted(): Array<SeriesData> {
+            if (this.titleSorting)
+                return this.series.sort((a, b) => a.name.localeCompare(b.name));
+            else
+                return this.series.sort((a, b) => b.name.localeCompare(a.name));
+        }
+    },
     async beforeRouteEnter(to: Route, from: Route, next: Next<StudioSeries>) {
+        LoadingBus.$emit("open");
         await seriesService.getAll(
             { showSeasons: true },
             (response: AxiosResponse) => {
@@ -53,11 +70,13 @@ import SeriesStatus from "@/enums/SeriesStatus";
                     array.push(new SeriesData(series));
                 });
                 next(vm => (vm.$data.series = array));
+                LoadingBus.$emit("close");
             },
             (error: AxiosResponse) => {
                 next(vm =>
                     vm.$data.errors.push(undefined, error.data as ServiceError)
                 );
+                LoadingBus.$emit("close");
             }
         );
     }
@@ -67,6 +86,7 @@ export default class StudioSeries extends Vue {
     errors = new Array<ServiceError>();
     SeriesStatus = SeriesStatus;
     activeSeries: number | null = null;
+    titleSorting = true;
 
     setData(series: SeriesData[], error: ServiceError) {
         error ? this.errors.push(error) : (this.series = series);
@@ -82,17 +102,30 @@ export default class StudioSeries extends Vue {
     padding: unset
     display: flex
     flex-direction: column
+    overflow: auto
+    height: 100vh
+    position: relative
 
     &__header
+        position: sticky
         padding: .75em 2em !important
         color: $white-second-color
+        background-color: $dark-color !important
+        top: 0
+        z-index: 999
+
+        &__info
+            display: flex
+            align-items: center
+            font-weight: bolder
+            color: $white-color
+            cursor: pointer
 
     &__element
         transition-duration: .2s
-        cursor: default
 
     &__element--not-active
-        opacity: .3
+        opacity: .5
 
     &__element, 
     &__header
@@ -112,17 +145,18 @@ export default class StudioSeries extends Vue {
                 margin-right: 1em
 
             &__name
-                letter-spacing: .05em
                 display: block
                 margin: 0
                 padding: 0
-                margin-bottom: .5em
+                margin-bottom: 1em
                 color: $white-color
 
             &__description
+                line-height: 1.5em
                 display: block
                 font-weight: lighter
                 color: $white-second-color
+                font-size: .9em
 
         &__status
             min-width: 10%
