0b6e14fbdd3156f00c2b44065845a17acaa29243
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-22 15:04:58 +0100

    Add loading modal in main studio view & styling changes

--- src/views/Studio.vue
+++ src/views/Studio.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class = "studio">
-        <v-navigation-column class = "studio__navigation-column"/>
-        <router-view class = "studio__views"/>
+    <div class="studio">
+        <v-navigation-column class="studio__navigation-column" />
+        <router-view class="studio__views" />
+        <v-modal-loading />
     </div>
 </template>
 
@@ -9,23 +10,25 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+import TheNavigationColumn from "@/components/Studio/TheNavigationColumn.vue";
+import ModalLoading from "@/components/Modals/ModalLoading.vue";
 
-import TheNavigationColumn from '@/components/Studio/TheNavigationColumn.vue';
 @Component({
     components: {
-        'v-navigation-column': TheNavigationColumn
+        "v-navigation-column": TheNavigationColumn,
+        "v-modal-loading": ModalLoading
     }
 })
 export default class Studio extends Vue {}
 </script>
 
 <style lang="sass" scoped>
-    .studio
-        display: flex
-        flex-direction: row
+.studio
+    display: flex
+    flex-direction: row
 
-        &__navigation-column
+    &__navigation-column
 
-        &__views
-            width: 100%
+    &__views
+        width: 100%
 </style>
