96d841eaf4fd2417c22e139bd7f8d424d922ef92
Maksymilian Łakomy <zommer128@gmail.com>
2020-03-23 18:41:21 +0100

    Add avatar to UserData

--- src/classes/UserData.ts
+++ src/classes/UserData.ts
@@ -1,9 +1,10 @@
 import RoleData from './RoleData';
 
-interface UserDataInterface {
+export interface UserDataInterface {
     id: string;
     email: string;
     username: string;
+    avatar: string;
     roles?: Array<RoleData>;
     accepted?: boolean;
 }
@@ -12,13 +13,15 @@ export default class UserData{
     readonly id: string;
     public email: string;
     public username: string;
+    public avatar: string;
     public roles?: Array<RoleData>;
     public accepted?: boolean;
 
-    constructor({id, email, username, roles, accepted}: UserDataInterface) {
+    constructor({id, email, username, avatar, roles, accepted}: UserDataInterface) {
         this.id = id;
         this.email = email;
         this.username = username;
+        this.avatar = avatar;
         this.roles = roles;
         this.accepted = accepted;
     }
