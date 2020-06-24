c1479592e9f1c95da748f1b5376b355e2cf1a2a8
Maksymilian Łakomy <zommer128@gmail.com>
2020-06-24 15:54:17 +0200

    Update DateInput to work with v-model & delete NumericInput

--- src/components/MaterialDesign/DateInput.vue
+++ src/components/MaterialDesign/DateInput.vue
@@ -8,32 +8,42 @@
     >
         <template v-slot:activator="{ on, attrs }">
             <v-text-field
-                v-model="syncedValue"
+                :value="value"
                 :label="label"
                 prepend-icon="event"
                 readonly
                 v-bind="attrs"
                 v-on="on"
+                :rules="rules"
+                outlined
             ></v-text-field>
         </template>
-        <v-date-picker v-model="syncedValue" no-title scrollable />
+        <v-date-picker :value="value" @change="$emit('change', $event)" no-title scrollable />
     </v-menu>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { PropSync, Prop } from 'vue-property-decorator';
+import {Prop } from 'vue-property-decorator';
 
-@Component
+@Component({
+    model: {
+        prop: 'value',
+        event: 'change'
+    }
+})
 export default class DateInput extends Vue {
     private menu = false;
 
     @Prop({required: false, type: String, default: ''})
     readonly label!: string;
 
-    @PropSync('value', {required: true, type: String})
-    syncedValue!: string;
+    @Prop({type: String, default: ""})
+    value!: string;
+
+    @Prop({type: Array, default: []})
+    rules!: Array<Function>;
     
 }
 </script>
