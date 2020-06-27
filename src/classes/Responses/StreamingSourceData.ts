760734b0981915e456fe4f422c08fd1b5bbfc584
Maksymilian Łakomy <zommer128@gmail.com>
2020-06-27 13:16:30 +0200

    Recreate service and responses for StreamingSources

--- src/classes/Responses/StreamingSourceData.ts
+++ src/classes/Responses/StreamingSourceData.ts
@@ -1,5 +1,23 @@
+import { LightEpisodeResponse } from './EpisodeResponses';
+
 export interface StreamingSourceResponse {
     id: string;
     name: string;
     path: string;
 }   
+
+export interface StreamingSourceRequestBody extends Pick<StreamingSourceResponse, 'name'>{
+    logo: Blob;
+}
+
+export interface StreamingSourceRemoveResponse {
+    associatedSeries: Array<LightEpisodeResponse>;
+    message: string;
+}
+
+export function createFormData(body: StreamingSourceRequestBody): FormData {
+    const formData = new FormData();
+    formData.set('name', body.name);
+    formData.set('logo', body.logo);
+    return formData;
+}
--- src/services/streamingSourcesService.ts
+++ src/services/streamingSourcesService.ts
@@ -1,5 +1,6 @@
 import Service from './Service';
 import { AxiosResponse, AxiosRequestConfig } from 'axios';
+import { StreamingSourceRequestBody, createFormData, StreamingSourceResponse, StreamingSourceRemoveResponse } from '@/classes/Responses/StreamingSourceData';
 
 const streamingSourceEndPoint = '/streamingSources';
 
@@ -12,24 +13,39 @@ const axiosRequestConfig: AxiosRequestConfig = {
     withCredentials: true
 }
 
-export default {
-    async create(body: FormData, callback: Function, errorCallback: Function) {
-        const response: AxiosResponse = await Service.post(streamingSourceEndPoint, body, { headers });
-        response.status === 201 ? callback() : errorCallback(response);
-    },
+export async function create(body: StreamingSourceRequestBody): Promise<StreamingSourceResponse> {
+    const formData = createFormData(body);
+    const response: AxiosResponse = await Service.post(streamingSourceEndPoint, formData, { headers });
+    return response.data as StreamingSourceResponse;
+}
 
-    async update(body: FormData, streamingSourceId: string, callback: Function, errorCallback: Function) {
-        const response: AxiosResponse = await Service.patch(`${streamingSourceEndPoint}/${streamingSourceId}`, body, axiosRequestConfig);
-        response.status === 200 ? callback(response) : errorCallback(response);
-    },
+export async function update(body: StreamingSourceRequestBody, id: string): Promise<StreamingSourceResponse> {
+    const formData = createFormData(body);
+    const response: AxiosResponse = await Service.patch(`${streamingSourceEndPoint}/${id}`, formData, axiosRequestConfig);
+    return response.data as StreamingSourceResponse;
+}
 
-    async get(streamingSourceId: string, callback: Function, errorCallback: Function) {
-        const response: AxiosResponse = await Service.get(`${streamingSourceEndPoint}/${streamingSourceId}`, axiosRequestConfig);
-        response.status === 200 ? callback(response) : errorCallback(response);
-    },
+export async function getAll(): Promise<Array<StreamingSourceResponse>> {
+    const response: AxiosResponse = await Service.get(`${streamingSourceEndPoint}`, axiosRequestConfig);
+    return response.data as Array<StreamingSourceResponse>;
+}
 
-    async delete(streamingSourceId: string, callback: Function, errorCallback: Function) {
-        const response: AxiosResponse = await Service.delete(`${streamingSourceEndPoint}/${streamingSourceId}`, axiosRequestConfig);
-        response.status === 204 ? callback(response) : errorCallback(response);
-    }
+export async function get(id: string) {
+    const response = await getAll();
+    const index = response.findIndex(source => source.id === id);
+    if (index !== -1)
+        return response[index];
+    throw new Error(`There is no streaming Source that match id: ${id}!`);
+}
+
+export async function remove (id: String): Promise<StreamingSourceRemoveResponse | null> {
+    const customConfig: AxiosRequestConfig = {};
+    Object.assign(customConfig, axiosRequestConfig);
+    customConfig.validateStatus = (status => status >= 200 || status < 300 || status === 400);
+
+    const response: AxiosResponse = await Service.delete(`${streamingSourceEndPoint}/${id}`, customConfig);
+    if (response.status === 400) 
+        return response.data as StreamingSourceRemoveResponse;
+    return null;
+    
 }
